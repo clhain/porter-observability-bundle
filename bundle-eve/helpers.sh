5ddd6a0fcf1e06be5e377e00d2ce3fd282f7bf24
Chris Hain <c.hain@f5.com>
2022-06-15 15:04:51 -0700

    remove root user related fields

--- bundle-eve/helpers.sh
+++ bundle-eve/helpers.sh
@@ -35,7 +35,7 @@ activate() {
 }
 
 render_volt_creds() {
-  cat /root/.volterra/creds.p12.b64 | base64 -d  > ${VOLT_API_P12_FILE}
+  cat /cnab/app/.volterra/creds.p12.b64 | base64 -d  > ${VOLT_API_P12_FILE}
 }
 
 echo "$@"
--- bundle-xc-ns-vk8s/helpers.sh
+++ bundle-xc-ns-vk8s/helpers.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 render_volt_creds() {
-  cat /root/.volterra/creds.p12.b64 | base64 -d  > ${VOLT_API_P12_FILE}
+  cat /cnab/app/.volterra/creds.p12.b64 | base64 -d  > ${VOLT_API_P12_FILE}
 }
 
 echo "$@"
