e186e0abed625e4457cfb22c60745b5196ba3354
Chris Hain <c.hain@f5.com>
2022-06-15 16:20:22 -0700

    troubleshooting

--- bundle-xc-ns-vk8s/helpers.sh
+++ bundle-xc-ns-vk8s/helpers.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 render_volt_creds() {
   mkdir -p /cnab/app/.volterra/
+  ls -lah /cnab/app/.volterra/
+  ls -lah /cnab/app/
   cat /cnab/app/.volterra/creds.p12.b64 | base64 -d  > ${VOLT_API_P12_FILE}
 }
 
